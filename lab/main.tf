490ef5130f774d01483b0455f02604ffc7c6f786
mrc <mrc78@protonmail.com>
2017-10-17 18:38:49 +0200

    terraform the lab

---
+++ lab/main.tf
@@ -0,0 +1,91 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "workshop_ami" {
+  most_recent = true
+
+  filter {
+    name   = "owner-id"
+    values = ["602636675831"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["beevalabs-workshop2017-*"]
+  }
+}
+
+resource "aws_vpc" "workshop_vpc" {
+  cidr_block           = "10.20.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "workshop_subnet" {
+  vpc_id                  = "${aws_vpc.workshop_vpc.id}"
+  cidr_block              = "10.20.0.0/16"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.workshop_vpc.id}"
+}
+
+resource "aws_iam_role" "mapd_sample_data_ro_role" {
+  name = "s3_readonly_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {"Service": "ec2.amazonaws.com"},
+    "Action": "sts:AssumeRole"
+  }
+}
+EOF
+}
+
+resource "aws_iam_policy" "mapd_sample_data_ro_policy" {
+  name        = "mapd_sample_data_ro_role"
+  description = "mapd_sample_data_ro_role"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:Get*",
+        "s3:List*"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::awssampledbuswest2"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = "${aws_iam_role.mapd_sample_data_ro_role.name}"
+  policy_arn = "${aws_iam_policy.mapd_sample_data_ro_policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "mapd_sample_data_ro_profile" {
+  name = "mapd_sample_data_ro_profile"
+  role = "${aws_iam_role.mapd_sample_data_ro_role.name}"
+}
+
+resource "aws_instance" "workshop_server" {
+  ami                  = "${data.aws_ami.workshop_ami.image_id}"
+  instance_type        = "p2.xlarge"
+  subnet_id            = "${aws_subnet.workshop_subnet.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.mapd_sample_data_ro_profile.id}"
+  key_name             = "mrc"
+  count                = 1
+
+  root_block_device = {
+    volume_size = 100
+  }
+}
