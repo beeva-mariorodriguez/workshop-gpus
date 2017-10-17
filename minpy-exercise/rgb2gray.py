1a8d313f86cf1032d2e3c835df064c06c206ac6a
Ubuntu <ubuntu@ip-172-31-3-239.us-east-2.compute.internal>
2017-10-17 15:53:49 +0000

    Fix gpu context problem

--- minpy-exercise/rgb2gray.py
+++ minpy-exercise/rgb2gray.py
@@ -4,12 +4,11 @@ from minpy.context import cpu, gpu
 import time
 from scipy import misc
 
-face = misc.imread('face.png')
-face_minpy = np.array(face)
-
 n = 5
 
 with cpu():
+    face = misc.imread('face.png')
+
     t1 = time.time()
     for i in xrange(n):
         gray_cpu = np.dot(face, np.array([0.299, 0.587, 0.114]))
@@ -17,6 +16,8 @@ with cpu():
 print("run on cpu: %.6f s/iter" % ((t2 - t1) / n))
 #
 with gpu(0):
+    face_minpy = np.array(face)
+
     t3 = time.time()
     for i in xrange(n):
         gray_gpu0 = np.dot(face_minpy, np.array([0.299, 0.587, 0.114]))
