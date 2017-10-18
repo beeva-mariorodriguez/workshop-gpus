354cdd7a8006a8a40d7b8d62857041afda44aff0
beeva-mariorodriguez <beeva-mariorodriguez@users.noreply.github.com>
2017-10-18 18:51:52 +0200

    use mapd 3.2.1
    mapd 3.2.4 dies on the first sample

--- scripts/start_mapd.sh
+++ scripts/start_mapd.sh
@@ -5,5 +5,4 @@ nvidia-docker run --name mapd \
     --publish 127.0.0.1:9092:9092 \
     --volume /mapd-storage/data:/mapd-storage/data \
     --volume /mapd-storage/ingest:/mapd-storage/ingest \
-    mapd/mapd-ce-cuda:v3.2.4
-
+    mapd/mapd-ce-cuda:v3.2.1
