db38e32c0bc716623e0f4af906791ceb50a87a56
beeva-mariorodriguez <beeva-mariorodriguez@users.noreply.github.com>
2017-10-18 12:00:30 +0200

    Update rgb2gray.py

--- minpy-exercise/rgb2gray.py
+++ minpy-exercise/rgb2gray.py
@@ -4,20 +4,22 @@ from minpy.context import cpu, gpu
 import time
 from scipy import misc
 
+print "starting exercise"
+
 n = 5
+face = misc.imread('face.png')
 
 with cpu():
-    face = misc.imread('face.png')
-
+    print "running on CPU..."
     t1 = time.time()
     for i in xrange(n):
         gray_cpu = np.dot(face, np.array([0.299, 0.587, 0.114]))
     t2 = time.time()
 print("run on cpu: %.6f s/iter" % ((t2 - t1) / n))
-#
+
 with gpu(0):
     face_minpy = np.array(face)
-
+    print "running on GPU..."
     t3 = time.time()
     for i in xrange(n):
         gray_gpu0 = np.dot(face_minpy, np.array([0.299, 0.587, 0.114]))
