0743cd987cbcf641c7f3a2dd1599882cf8b8bbb0
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-10-18 18:01:40 +0200

    download all mapd sample files

--- scripts/download_mapd_sample_data.sh
+++ scripts/download_mapd_sample_data.sh
@@ -7,4 +7,13 @@ do
     curl "http://awssampledbuswest2.s3.amazonaws.com/ssbgz/lineorder000${i}_part_00.gz" | \
         gunzip > "${DEST}/lineorder000${i}_part_00"
 done
+for i in {0..3}
+do
+    curl "http://awssampledbuswest2.s3.amazonaws.com/ssbgz/part000${i}_part_00.gz" | \
+        gunzip > "${DEST}/part000${i}_part_00" 
+done
+curl "http://awssampledbuswest2.s3.amazonaws.com/ssbgz/customer0002_part_00.gz" | \
+    gunzip > "${DEST}/customer0002_part_00"
+
+cp dwdate.tbl "$DEST"
 
